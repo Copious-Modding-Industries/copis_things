667f7d6bd9d8c9bb0f883af9368c0af4323e1e0f
Human <54731850+Ramiels@users.noreply.github.com>
2022-02-06 02:43:55 -0500

    saved the world

--- files/actions.lua
+++ files/actions.lua
@@ -1,12 +1,12 @@
 	table.insert(actions,
 	{
-	id          		= "copis_things_PSYCHIC_SHOT",
+	id          		= "COPIS_THINGS_PSYCHIC_SHOT",
 	name 				= "Psychic shot",
 	description 		= "Causes the projectile to be controlled telekinetically",
 	sprite 				= "mods/copis_things/files/sprites/spell_gui/psychic_shot.png",
 	type 				= ACTION_TYPE_MODIFIER,
-	spawn_level         = "2,3,4,5,6", 
-	spawn_probability   = "0.3,0.4,0.5,0.6,0.6", 
+	spawn_level         = "2,3,4,5,6",
+	spawn_probability   = "0.3,0.4,0.5,0.6,0.6",
 	price 				= 150,
 	mana 				= 15,
     action 		= function()
