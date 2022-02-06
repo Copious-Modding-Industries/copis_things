9f03d5c7fda59295052bd4ea188f22382f736751
Human <54731850+Ramiels@users.noreply.github.com>
2022-02-06 04:47:47 -0500

    added

--- files/actions.lua
+++ files/actions.lua
@@ -14,3 +14,23 @@
 			draw_actions( 1, true )
 		end
 	})
+	table.insert(actions,
+	{
+	id                 = "COPIS_THINGS_STAB",
+	name               = "Stab",
+	description        = "A little stabby stabby",
+	sprite             = "mods/copis_things/files/sprites/spell_gui/stab.png",
+	related_projectiles	= {"mods/copis_things/files/entities/projectiles/stab.xml"},
+	type               = ACTION_TYPE_PROJECTILE,
+	spawn_level        = "0,1,3,5",
+	spawn_probability  = "0.9,0.9,0.9,0.9",
+	price              = 110,
+	mana               = 15,
+	action 		= function()
+			add_projectile("mods/copis_things/files/entities/projectiles/stab.xml")
+			c.fire_rate_wait = c.fire_rate_wait + 7
+			c.screenshake = c.screenshake + 0.7
+			c.spread_degrees = c.spread_degrees - 2.0
+			c.damage_critical_chance = c.damage_critical_chance + 15
+		end,
+	})
--- init.lua
+++ init.lua
@@ -54,18 +54,4 @@ end
 
 -- This code runs when all mods' filesystems are registered
 
-
-
-
-
-ModLuaFileAppend( "data/scripts/gun/gun_actions.lua", "mods/example/files/actions.lua" ) -- Basically dofile("mods/example/files/actions.lua") will appear at the end of gun_actions.lua
-ModMagicNumbersFileAdd( "mods/example/files/magic_numbers.xml" ) -- Will override some magic numbers using the specified file
-ModRegisterAudioEventMappings( "mods/example/files/audio_events.txt" ) -- Use this to register custom fmod events. Event mapping files can be generated via File -> Export GUIDs in FMOD Studio.
-ModMaterialsFileAdd( "mods/example/files/materials_rainbow.xml" ) -- Adds a new 'rainbow' material to materials
-ModLuaFileAppend( "data/scripts/items/potion.lua", "mods/example/files/potion_appends.lua" )
-
-
-
-
-
---print("Example mod init done")
+GamePrint("Cope")
