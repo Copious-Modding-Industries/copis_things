cd084bf3a92d83fa9568fb6685d951b18b220c37
Human <54731850+Ramiels@users.noreply.github.com>
2022-02-10 00:59:20 -0500

    added sword edge and edge book

--- files/actions.lua
+++ files/actions.lua
@@ -18,14 +18,14 @@
 	{
 	id                 = "COPIS_THINGS_STAB",
 	name               = "Stab",
-	description        = "A little stabby stabby",
+	description        = "A short ranged melee thrust",
 	sprite             = "mods/copis_things/files/sprites/spell_gui/stab.png",
 	related_projectiles	= {"mods/copis_things/files/entities/projectiles/stab.xml"},
 	type               = ACTION_TYPE_PROJECTILE,
-	spawn_level        = "0,1,3,5",
-	spawn_probability  = "0.9,0.9,0.9,0.9",
-	price              = 110,
-	mana               = 15,
+	spawn_level        = "0,0,0,0",
+	spawn_probability  = "0,0,0,0",
+	price              = 0,
+	mana               = 0,
 	action 		= function()
 			add_projectile("mods/copis_things/files/entities/projectiles/stab.xml")
 			c.fire_rate_wait = c.fire_rate_wait + 7
@@ -34,3 +34,84 @@
 			c.damage_critical_chance = c.damage_critical_chance + 15
 		end,
 	})
+
+	table.insert(actions,
+	{
+	id                 = "COPIS_THINGS_TWISTED_SWORD_THROW",
+	name               = "Sword throw",
+	description        = "Throw a rapidly spinning sword clone",
+	sprite             = "mods/copis_things/files/sprites/spell_gui/twisted_throw.png",
+	related_projectiles	= {"mods/copis_things/files/entities/projectiles/twisted_throw.xml"},
+	type               = ACTION_TYPE_PROJECTILE,
+	spawn_level        = "0,0,0,0",
+	spawn_probability  = "0,0,0,0",
+	price              = 0,
+	mana               = 0,
+	action 		= function()
+			add_projectile("mods/copis_things/files/entities/projectiles/twisted_throw.xml")
+			c.screenshake = c.screenshake + 0.7
+			c.spread_degrees = c.spread_degrees - 2.0
+			c.damage_critical_chance = c.damage_critical_chance + 15
+			c.fire_rate_wait = c.fire_rate_wait + 40
+			shot_effects.recoil_knockback = shot_effects.recoil_knockback + 30.0
+			c.damage_projectile_add = c.damage_projectile_add + 0.2
+		end,
+	})
+
+	table.insert(actions,
+	{
+		id          = "COPIS_THINGS_LUNGE",
+		name 		= "Lunge",
+		description = "Launch yourself forwards with a burst of speed",
+		sprite 		= "data/ui_gfx/gun_actions/recoil.png",
+		sprite_unidentified = "data/ui_gfx/gun_actions/recoil_unidentified.png",
+		type 		= ACTION_TYPE_MODIFIER,
+		spawn_level = "2,4",
+		spawn_probability = "0.6,0.6",
+		price = 100,
+		mana = 5,
+		action 		= function()
+			shot_effects.recoil_knockback = shot_effects.recoil_knockback + 200.0
+			draw_actions( 1, true )
+		end,
+	})
+
+	table.insert(actions,
+	{
+		id          = "COPIS_THINGS_SHADOWTENTACLE",
+		name 		= "Shadow Apparition",
+		description = "Release a lash of pure darkness which spreads from it's victims",
+		sprite 		= "mods/copis_things/files/sprites/spell_gui/shadowtentacle.png",
+		related_projectiles	= {"mods/copis_things/files/projectiles/shadowtentacle.xml"},
+		type 		= ACTION_TYPE_PROJECTILE,
+		spawn_level        = "0,0,0,0",
+		spawn_probability  = "0,0,0,0",
+		price = 170,
+		mana = 30,
+		action 		= function()
+			add_projectile("mods/copis_things/files/projectiles/shadowtentacle.xml")
+			c.fire_rate_wait = c.fire_rate_wait + 5
+		end
+	})
+
+
+	table.insert(actions,
+{
+    id          = "BLOODTENTACLE",
+    name 		= "$action_bloodtentacle",
+    description = "$actiondesc_bloodtentacle",
+    spawn_requires_flag = "card_unlocked_pyramid",
+    sprite 		= "data/ui_gfx/gun_actions/bloodtentacle.png",
+    sprite_unidentified = "data/ui_gfx/gun_actions/tentacle_unidentified.png",
+    related_projectiles	= {"data/entities/projectiles/deck/bloodtentacle.xml"},
+    type 		= ACTION_TYPE_PROJECTILE,
+    spawn_level                       = "3,4,5,6", -- TENTACLE
+    spawn_probability                 = "0.2,0.5,1,1", -- TENTACLE
+    price = 170,
+    mana = 30,
+    --max_uses = 40,
+    action 		= function()
+        add_projectile("data/entities/projectiles/deck/bloodtentacle.xml")
+        c.fire_rate_wait = c.fire_rate_wait + 20
+    end,
+})
--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -- all functions below are optional and can be left out
 ModLuaFileAppend("data/scripts/gun/gun_actions.lua", "mods/copis_things/files/actions.lua")
+-- ModLuaFileAppend("data/scripts/gun/gun_actions.lua", "mods/copis_things/files/actions_removed.lua")
 
 --[[
 
@@ -15,15 +16,20 @@ function OnModPostInit()
 	print("Mod - OnModPostInit()") -- Then this is called for all mods
 end
 ]]--
+
 function OnPlayerSpawned( player_entity ) -- This runs when player entity has been created
 	GamePrint( "OnPlayerSpawned() - Player entity id: " .. tostring(player_entity) )
-	EntityLoad("mods/copis_things/files/entities/special_wands/hammer/hammer.xml", 20, -40)
-	EntityLoad("mods/copis_things/files/entities/special_wands/earthworm/earthworm.xml", 40, -40)
-	EntityLoad("mods/copis_things/files/entities/special_wands/tachamis/tachamis.xml", 60, -40)
-	EntityLoad("mods/copis_things/files/entities/special_wands/frostburn/frostburn.xml", 80, -40)
-	EntityLoad("mods/copis_things/files/entities/special_wands/hellfire/hellfire.xml", 100, -40)
-	EntityLoad("mods/copis_things/files/entities/special_wands/energy_blaster/energy_blaster.xml", 120, -40)
-	EntityLoad("mods/copis_things/files/entities/special_wands/viper/viper.xml", 140, -40)
+	EntityLoad("mods/copis_things/files/entities/special_wands/hammer/hammer.xml", 20, -80)
+	EntityLoad("mods/copis_things/files/entities/special_wands/earthworm/earthworm.xml", 40, -80)
+	EntityLoad("mods/copis_things/files/entities/special_wands/tachamis/tachamis.xml", 60, -80)
+	EntityLoad("mods/copis_things/files/entities/special_wands/frostburn/frostburn.xml", 80, -80)
+	EntityLoad("mods/copis_things/files/entities/special_wands/hellfire/hellfire.xml", 100, -80)
+	EntityLoad("mods/copis_things/files/entities/special_wands/energy_blaster/energy_blaster.xml", 120, -80)
+	EntityLoad("mods/copis_things/files/entities/special_wands/viper/viper.xml", 140, -80)
+	EntityLoad("mods/copis_things/files/entities/special_wands/twig/twig.xml", 160, -80)
+	EntityLoad("mods/copis_things/files/entities/special_wands/lonk_sword/lonk_sword.xml", 180, -80)
+	EntityLoad("mods/copis_things/files/entities/special_wands/twisted/twisted.xml", 200, -80)
+	EntityLoad("mods/copis_things/files/entities/special_wands/tome_edgy/tome_edgy.xml", 220, -80)
 end
 
 
