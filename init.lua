64869785e81e6a27e219043ee7a550c9e23af077
Mir Sole <65946597+ZacIsBurnt@users.noreply.github.com>
2022-02-25 15:35:38 +1100

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@
 ModLuaFileAppend("data/scripts/gun/gun_actions.lua", "mods/copis_things/files/actions.lua")
 -- ModLuaFileAppend("data/scripts/gun/gun_actions.lua", "mods/copis_things/files/actions_removed.lua")
 
+--mr copium is very cool
+
 --[[
 
 function OnModPreInit()
