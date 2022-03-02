63b0949388d6a3f090f039377e2ae424f75dde49
Human <54731850+Ramiels@users.noreply.github.com>
2022-03-02 12:24:24 -0500

    Unfinished zap

---
+++ files/scripts/projectiles/zap.lua
@@ -0,0 +1,12 @@
+local entity = GetUpdatedEntityID();
+local x, y = EntityGetTransform( entity );
+local velocity = EntityGetFirstComponentIncludingDisabled( entity, "VelocityComponent" );
+if velocity ~= nil then
+    -- wtf is that
+    local zap_frame = EntityAdjustVariableNumber( entity, "gkbrkn_zap_frames", 0.0, function(value) return tonumber(value) + 1; end ) or 0;         
+    local vx,vy = ComponentGetValue2( velocity, "mVelocity", vx, vy );
+    local scale = math.min( 1, 0.25 * zap_frame * 0.5 );
+    local angle = math.atan2( vy, vx ) + (math.random() - 0.5) * math.pi * scale;
+    local magnitude = math.sqrt( vx * vx + vy * vy );
+    ComponentSetValue2( velocity, "mVelocity", math.cos( angle ) * magnitude, math.sin( angle ) * magnitude );
+end
