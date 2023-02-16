639133435e2cce04f07fe00b854f0ad538858c9c
Human <54731850+Ramiels@users.noreply.github.com>
2023-02-16 01:01:04 -0500

    @ofoxsmith make this not lag

--- files/scripts/projectiles/barrier_arc.lua
+++ files/scripts/projectiles/barrier_arc.lua
@@ -1,31 +1,39 @@
 dofile_once("data/scripts/lib/utilities.lua")
 local entity_id = GetUpdatedEntityID()
-
+-- Avoid indexer projectiles running
 if EntityGetName(entity_id) ~= "separator" then
     local caststate = nil
-    do -- Get cast state
+    do -- Get cast state of self
         local projcomp = EntityGetFirstComponentIncludingDisabled(entity_id, "ProjectileComponent")
         local desc = ComponentObjectGetValue2(projcomp, "config", "action_description")
         local i, j = string.find(desc, "\nCASTSTATE|([a-zA-Z0-9]*)\n")
         caststate = (string.sub(desc, i, j))
     end
 
+    -- Setup
     local function lerp(a, b, t) return a * (1 - t) + b * t end
     local pos_x, pos_y = EntityGetTransform(entity_id)
 
+    -- Go over player projectiles
     local player_projectiles = EntityGetInRadiusWithTag(pos_x, pos_y, 256, "projectile_player") or {}
     for index = 1, #player_projectiles do
         local target = player_projectiles[index]
+        -- If it's tagged as barrier arc (see barrier_arc_init.lua) do..
         if EntityHasTag(target, "BARRIER_ARC") then
             local projcomp = EntityGetFirstComponentIncludingDisabled(target, "ProjectileComponent")
             local desc = ComponentObjectGetValue2(projcomp, "config", "action_description")
             local i, j = string.find(desc, "\nCASTSTATE|([a-zA-Z0-9]*)\n")
+            -- Check if it has caststate in it
             if (i or j) ~= nil then
                 local target_caststate = (string.sub(desc, i, j))
+                -- if it matches this projectile's cast state then..
                 if target_caststate == caststate then
                     local tx, ty = EntityGetTransform(target)
+                    -- calculate distance
                     local distance = math.sqrt(math.pow(tx - pos_x, 2) + math.pow(ty - pos_y, 2))
+                    -- every 12 units of distance place a wall piece
                     for dist = 1, distance, 12 do
+                        -- lerp the position so we go between the 2 shots
                         local px = lerp(pos_x, tx, (1 / distance) * dist)
                         local py = lerp(pos_y, ty, (1 / distance) * dist)
                         local wall_piece = shoot_projectile_from_projectile(
@@ -34,6 +42,7 @@ if EntityGetName(entity_id) ~= "separator" then
                             px, py,
                             0, 0
                         )
+                        -- Set it's lifetime to 2f and make it not be caught in the checks to prevent frame issue
                         local projectile = EntityGetFirstComponentIncludingDisabled(wall_piece, "ProjectileComponent")
                         if projectile ~= nil then
                             ComponentSetValue2(projectile, "lifetime", 2);
