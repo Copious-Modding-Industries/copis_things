2b1222306976c35ba9576a3fcf2337d9ba9a16af
Human <54731850+Ramiels@users.noreply.github.com>
2023-04-06 18:08:01 -0400

    readme changes

--- README.md
+++ README.md
@@ -1,12 +1,34 @@
 # Copi's Things
-A recollection of changes and additions to Noita.
-
-~~This mod contains an in-game config menu found at the top right of the screen. Use it to toggle options and custom content.~~
-GUI will be added in a later update
-
+## [Wiki (heavily WIP)](https://noita.wiki.gg/wiki/Mod:Copi%27s_Things)
+A series of changes and additions to Noita.
 
 # Install Instructions
 
  - Press the *[Download Zip](https://github.com/Ramiels/copis_things/archive/refs/heads/main.zip)* button
  - Extract into your noita mod folder (`C:/Program Files (x86)/Steam/steamapps/common/Noita/mods/` for steam users)
  - Rename `copis_things-main` to `copis_things`
+
+# Compatible Mods (*a-z*)
+
+**Apotheosis**
+ - Changes:
+   - Spells added to reward pools
+ - Download Links:
+   - [Github]((https://github.com/Conga0/Apotheosis))
+
+**Chemical Curiosities**
+ - Changes:
+   - Materials added to Chaos material spell pools
+ - Download Links:
+   - [Github](https://github.com/Squirrelly13/Hydroxide)
+   - [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=2866701037)
+   - [ModWorkshop](https://modworkshop.net/mod/39678)
+
+**More Creeps and Weirdos**
+ - Changes:
+   - Spells added to reward pools
+ - Download Links:
+   - [Github](https://github.com/Conga0/Mo_Creeps)
+   - [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=2879253717)
+   - [ModWorkshop](https://modworkshop.net/mod/39990)
+
--- test.lua
+++ test.lua
@@ -1,3 +1,83 @@
+-- NOTE: keep this 'enum' in sync with GunActionType in gun_component.h
+ACTION_TYPE_PROJECTILE	= 0
+ACTION_TYPE_STATIC_PROJECTILE = 1
+ACTION_TYPE_MODIFIER	= 2
+ACTION_TYPE_DRAW_MANY	= 3
+ACTION_TYPE_MATERIAL	= 4
+ACTION_TYPE_OTHER		= 5
+ACTION_TYPE_UTILITY		= 6
+ACTION_TYPE_PASSIVE		= 7
+
+local c = {
+    action_id = "",
+    action_name = "",
+    action_description = "",
+    action_sprite_filename = "",
+    action_unidentified_sprite_filename = "data/ui_gfx/gun_actions/unidentified.png",
+    action_type = ACTION_TYPE_PROJECTILE,
+    action_spawn_level = "",
+    action_spawn_probability = "",
+    action_spawn_requires_flag = "",
+    action_spawn_manual_unlock = false,
+    action_max_uses = -1,
+    custom_xml_file = "",
+    action_mana_drain = 10,
+    action_is_dangerous_blast = false,
+    action_draw_many_count = 0,
+    action_ai_never_uses = false,
+    action_never_unlimited = false,
+    state_shuffled = false,
+    state_cards_drawn = 0,
+    state_discarded_action = false,
+    state_destroyed_action = false,
+    fire_rate_wait = 0,
+    speed_multiplier = 1.0,
+    child_speed_multiplier = 1.0,
+    dampening = 1,
+    explosion_radius = 0,
+    spread_degrees = 0,
+    pattern_degrees = 0,
+    screenshake = 0,
+    recoil = 0,
+    damage_melee_add = 0.0,
+    damage_projectile_add = 0.0,
+    damage_electricity_add = 0.0,
+    damage_fire_add = 0.0,
+    damage_explosion_add = 0.0,
+    damage_ice_add = 0.0,
+    damage_slice_add = 0.0,
+    damage_healing_add = 0.0,
+    damage_curse_add = 0.0,
+    damage_drill_add = 0.0,
+    damage_critical_chance = 0,
+    damage_critical_multiplier = 0.0,
+    explosion_damage_to_materials = 0,
+    knockback_force = 0,
+    reload_time = 0,
+    lightning_count = 0,
+    material = "",
+    material_amount = 0,
+    trail_material = "",
+    trail_material_amount = 0,
+    bounces = 0,
+    gravity = 0,
+    light = 0,
+    blood_count_multiplier = 1.0,
+    gore_particles = 0,
+    ragdoll_fx = 0,
+    friendly_fire = false,
+    physics_impulse_coeff = 0,
+    lifetime_add = 0,
+    sprite = "",
+    extra_entities = "",
+    game_effect_entities = "",
+    sound_loop_tag = "",
+    projectile_file = ""
+}
+
+-- Load Ent + add velocity
+
+-- Extra Ents
 do
     local projectile = nil
     for extra_entity in string.gmatch(c.extra_entities, '([^,]+)') do
@@ -6,73 +86,10 @@ do
     end
 end
 
-
-
-
-dofile_once("data/scripts/lib/utilities.lua")
--- load random stuff
-local entity_id    = GetUpdatedEntityID()
-local pos_x, pos_y = EntityGetTransform( entity_id )
-local self_velcomp = EntityGetFirstComponentIncludingDisabled( entity_id, "VelocityComponent" )
-local self_projcomp = EntityGetFirstComponentIncludingDisabled(entity_id, "ProjectileComponent")
-if self_projcomp and self_velcomp then
-    local vel_x, vel_y = ComponentGetValueVector2( self_velcomp, "mVelocity" )
-    local currbiome = tostring(BiomeMapGetName( pos_x, pos_y ))
-    local shooter = ComponentGetValue2(self_projcomp, "mWhoShot")
-
-    local biome_projectiles = {
-        grenades = {
-            ["Blast Pit"]       = {"mine", 265, 0.12},
-            ["$biome_snowcave"] = {"ice", 150, 0.0},
-        }
-    }
-
-    -- Set projs
-    local projectiles = {
-        {"light_bullet", 800, 0.10},
-        {"bullet", 625, 0.8},
-        {"bubbleshot", 250, 0.40},
-        {"spitter", 500, 0.40},
-        {"rubber_ball", 700, 0.14},
-        biome_projectiles["grenades"][currbiome] or {"grenade", 265, 0.12},
-    }
-
-    -- make all projectiles from one shooter the same kind
-    math.randomseed(entity_id)
-
-    -- pick projectile based on the shooter id
-    local projectile = projectiles[math.random(1, #projectiles)]
-
-    -- re-randomize for spread and velocity
-    math.randomseed(GameGetFrameNum(), entity_id)
-
-    -- variation of speed above
-    local var = projectile[3]
-
-    -- 'length' is the base speed, it is multiplied by the variation value
-    local length = projectile[2] * math.random(1 - var/2, 1 + var/2)
-
-    -- set angle to fire at
-    local angle = math.atan2( vel_y, vel_x ) + math.rad(math.random(-10,10))
-
-    -- turn that into velocity
-    vel_x = math.cos(angle) * length
-    vel_y = math.sin(angle) * length
-
-    -- offset pos
-    pos_x = pos_x + math.random(-2, 2)
-    pos_y = pos_y + math.random(-1, 1)
-
-    -- shoot to kill
-    do  -- Shooty logic
-        local fired_shot = EntityLoad( table.concat{"data/entities/projectiles/mimic_", projectile[1], ".xml"}, x, y )
-        local fired_projcomp = EntityGetFirstComponentIncludingDisabled(fired_shot, "ProjectileComponent")
-        local fired_velcomp = EntityGetFirstComponentIncludingDisabled(fired_shot, "ProjectileComponent")
-        if fired_projcomp and fired_velcomp then
-            ComponentSetValue2(fired_projcomp, "mWhoShot", shooter)
-            ComponentSetValue2(fired_projcomp, "mEntityThatShot", entity_id )
-            GameShootProjectile( shooter, pos_x, pos_y, pos_x+vel_x, pos_y+vel_y, fired_shot, true, entity_id )
-            ComponentSetValueVector2( fired_velcomp, "mVelocity", vel_x, vel_y)
-        end
-    end
+-- todo: damage typed additions
+local damage = ComponentGetValue2( projcomp, "damage" )
+ComponentSetValue2( projcomp, "damage", damage * 1.5 )
+for _, type in ipairs(damage_types) do
+    local type_damage = ComponentObjectGetValue2(projcomp, "damage_by_type", type)
+    ComponentObjectSetValue2(projcomp, "damage_by_type", type, type_damage * 1.5)
 end
