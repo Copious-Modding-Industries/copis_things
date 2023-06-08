a7bad22ac5785d512be29d0fef08b3d86723bea9
Evaisa <evagiacosa1@gmail.com>
2023-06-08 20:26:37 +0200

    Make invincibility frames a bit less of a crybaby

--- files/scripts/perk/damage_received/invincibility_frames.lua
+++ files/scripts/perk/damage_received/invincibility_frames.lua
@@ -2,13 +2,22 @@ function damage_received( damage, message, entity_thats_responsible, is_fatal, p
     local entity = GetUpdatedEntityID();
     if damage > 0 then
         if entity_thats_responsible ~= entity then
-            local vsc = EntityGetFirstComponentIncludingDisabled(entity, "VariableStorageComponent", "invincibility_frames");
-            local invincibility_duration = ComponentGetValue2(vsc, "value_int")
-            local dmcs = EntityGetComponentIncludingDisabled( entity, "DamageModelComponent" ) or {}
-            for _, dmc in ipairs(dmcs) do
-                local current_invincibility_frames = ComponentGetValue2( dmc, "invincibility_frames" )
-                if current_invincibility_frames <= 0 then
-                    ComponentSetValue2( dmc, "invincibility_frames", invincibility_duration )
+            local vscs = EntityGetComponent(entity, "VariableStorageComponent") or {}
+            local vsc = nil
+            for i=1, #vscs do
+                if ComponentGetValue2( vscs[i], "name" ) == "invincibility_frames" then
+                    vsc = vscs[i]
+                    break
+                end
+            end
+            if(vsc)then
+                local invincibility_duration = ComponentGetValue2(vsc, "value_int")
+                local dmcs = EntityGetComponentIncludingDisabled( entity, "DamageModelComponent" ) or {}
+                for _, dmc in ipairs(dmcs) do
+                    local current_invincibility_frames = ComponentGetValue2( dmc, "invincibility_frames" )
+                    if current_invincibility_frames <= 0 then
+                        ComponentSetValue2( dmc, "invincibility_frames", invincibility_duration )
+                    end
                 end
             end
         end
